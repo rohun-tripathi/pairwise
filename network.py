fa65c9cc132072be079cf746f58a26b47c191df0
rohun-tripati <tripati.riot@gmail.com>
2018-02-21 10:22:10 -0500

    refactor for debugging

---
+++ network.py
@@ -0,0 +1,78 @@
+import torch
+from torch.autograd import Variable
+
+from pairwise_train import print_and_log
+from util.function_library import AverageMeter, check_accuracy
+
+import shutil
+
+
+def train(train_loader, model, criterion, optimizer, epoch):
+    model.train()
+    accuracy_meter = AverageMeter()
+
+    training_loss = AverageMeter()
+    for index, data in enumerate(train_loader):
+
+        if index % 1000 == 0 and index > 0:
+            print_and_log('Training', 'iter', epoch, 'batch_number', index, "loss", training_loss.avg)
+
+        image, point_1_img, point_2_img, label, _ = data
+        image, point_1_img, point_2_img, label = \
+            Variable(image), Variable(point_1_img), Variable(point_2_img), Variable(label)
+
+        if torch.cuda.is_available():
+            image, point_1_img, point_2_img, label = image.cuda(), point_1_img.cuda(), point_2_img.cuda(), label.cuda()
+
+        output = model(image, point_1_img, point_2_img)
+
+        loss = criterion(output, label)
+        training_loss.update(loss.data[0], image.size(0))
+
+        accuracy = check_accuracy(output, label)
+        accuracy_meter.update(accuracy)
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+    print_and_log("Epoch", str(epoch), "train_accuracy", str(accuracy_meter.avg))
+
+
+def evaluate(test_loader, model):
+    model.eval()
+    accuracy_meter, whdr_err_meter = AverageMeter(), AverageMeter()
+    number_of_batches = len(test_loader)
+
+    for index, data in enumerate(test_loader):
+        if index % 1000 == 0:
+            print_and_log("Eval", "batch_number", index, "total", number_of_batches)
+
+        image, point_1_img, point_2_img, label, weight = data
+        image, point_1_img, point_2_img, label = \
+            Variable(image, volatile=True), Variable(point_1_img, volatile=True), Variable(point_2_img, volatile=True),\
+            Variable(label, volatile=True)
+
+        if torch.cuda.is_available():
+            image, point_1_img, point_2_img = image.cuda(), point_1_img.cuda(), point_2_img.cuda()
+
+        output = model(image, point_1_img, point_2_img).cpu()
+
+        accuracy = check_accuracy(output, label)
+        # whdr_error = check_whdr(output, label, weight)
+        # whdr_err_meter.update(whdr_error)
+        accuracy_meter.update(accuracy)
+
+    model.train()
+    return accuracy_meter.avg, whdr_err_meter.avg
+
+
+# Presently we have no annealing.
+def update_learning_rate(optimizer, step_count, max_steps, bs):
+    ...
+
+
+def save_checkpoint(state, is_best, filename='trained_models/checkpoint.pth.tar'):
+    torch.save(state, filename)
+    if is_best:
+        shutil.copyfile(filename, 'trained_models/model_best.pth.tar')
--- pairwise_train.py
+++ pairwise_train.py
@@ -1,17 +1,13 @@
-from torch.autograd import Variable
-from torch.utils.data import DataLoader
-import models.multi_stream_model as multi_stream_model
-import torch
-import torchvision.transforms as transforms
-import shutil
-
-from util.function_library import AverageMeter, check_accuracy, check_whdr
-
-from data.pair_wise_image import IIWDataset
-
+import datetime
 import logging
 
-import datetime
+import torch
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+
+import models.multi_stream_model as multi_stream_model
+from data.pair_wise_image import IIWDataset
+from network import train, update_learning_rate, evaluate, save_checkpoint
 
 exp_name = "128_on_two_GPUS"
 filename = 'logs/' + exp_name + '_experiment-{date:%d_%m_%H_%M_%S}.log'.format(date=datetime.datetime.now())
@@ -26,77 +22,6 @@ def print_and_log(*inputs):
         parts = "_".join([str(x) for x in inputs])
         print(parts)
         logging.info(parts)
-
-
-def train(train_loader, model, criterion, optimizer, epoch):
-    model.train()
-    accuracy_meter = AverageMeter()
-
-    training_loss = AverageMeter()
-    for index, data in enumerate(train_loader):
-
-        if index % 1000 == 0 and index > 0:
-            print_and_log('Training epoch, iter- ', epoch, index, "loss", training_loss.avg)
-
-        image, point_1_img, point_2_img, label, _ = data
-        image, point_1_img, point_2_img, label = \
-            Variable(image), Variable(point_1_img), Variable(point_2_img), Variable(label)
-
-        if torch.cuda.is_available():
-            image, point_1_img, point_2_img, label = image.cuda(), point_1_img.cuda(), point_2_img.cuda(), label.cuda()
-
-        output = model(image, point_1_img, point_2_img)
-
-        loss = criterion(output, label)
-        training_loss.update(loss.data[0], image.size(0))
-
-        accuracy = check_accuracy(output, label)
-        accuracy_meter.update(accuracy)
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-    print_and_log("Epoch", str(epoch), "train_accuracy", str(accuracy_meter.avg))
-
-
-def evaluate(test_loader, model):
-    model.eval()
-    accuracy_meter, whdr_err_meter = AverageMeter(), AverageMeter()
-    number_of_batches = len(test_loader)
-
-    for index, data in enumerate(test_loader):
-        if index % 1000 == 0:
-            print_and_log("Eval", "batch_number", index, "total", number_of_batches)
-
-        image, point_1_img, point_2_img, label, weight = data
-        image, point_1_img, point_2_img, label = \
-            Variable(image, volatile=True), Variable(point_1_img, volatile=True), Variable(point_2_img, volatile=True),\
-            Variable(label, volatile=True)
-
-        if torch.cuda.is_available():
-            image, point_1_img, point_2_img = image.cuda(), point_1_img.cuda(), point_2_img.cuda()
-
-        output = model(image, point_1_img, point_2_img).cpu()
-
-        accuracy = check_accuracy(output, label)
-        # whdr_error = check_whdr(output, label, weight)
-        # whdr_err_meter.update(whdr_error)
-        accuracy_meter.update(accuracy)
-
-    model.train()
-    return accuracy_meter.avg, whdr_err_meter.avg
-
-
-# Presently we have no annealing.
-def update_learning_rate(optimizer, step_count, max_steps, bs):
-    ...
-
-
-def save_checkpoint(state, is_best, filename='trained_models/checkpoint.pth.tar'):
-    torch.save(state, filename)
-    if is_best:
-        shutil.copyfile(filename, 'trained_models/model_best.pth.tar')
 
 
 # Paper defined values
